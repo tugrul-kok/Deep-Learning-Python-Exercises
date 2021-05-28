a40943e0e5c0fbc5499390dd0ecf0208df17a0e7
tugrul-kok <tugrulkok@gmail.com>
2021-05-28 12:18:49 +0300

    download

--- bounAI-tasks/bounAI-task4-CNN/download_data.sh
+++ bounAI-tasks/bounAI-task4-CNN/download_data.sh
@@ -1,10 +1,11 @@
 if [ ! -f data/ ]
 then
 
-#This will download data from https://drive.google.com/drive/folders/1_xOtuwg8jmmoQ4fK_PUBj5c8MCttEnUB?usp=sharing
-
-pip3 install gdown
-gdown https://drive.google.com/u/0/uc?id=1_xOtuwg8jmmoQ4fK_PUBj5c8MCttEnUB?usp=sharing
+mkdir data
+cd data
+wget -cq http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
+tar -xvf annotation.tar
+rm annotation.tar
 
 else
   echo "data exists"
